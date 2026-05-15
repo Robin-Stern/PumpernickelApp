711553e737406260c2569c2578aff7b15eb9f89c
Bernhard <BerZontax@gmail.com>
2026-05-15 21:03:05 +0200

    fix: add missing Column import in MacroRow.kt

--- androidApp/src/androidMain/kotlin/com/pumpernickel/android/ui/screens/MacroRow.kt
+++ androidApp/src/androidMain/kotlin/com/pumpernickel/android/ui/screens/MacroRow.kt
@@ -3,6 +3,7 @@ package com.pumpernickel.android.ui.screens
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
